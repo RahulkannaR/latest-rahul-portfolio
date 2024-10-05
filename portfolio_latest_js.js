90565f6954d7da8b539e05854f912d993bf90e41
RahulkannaR <146975427+RahulkannaR@users.noreply.github.com>
2024-10-05 16:02:58 +0530

    Add files via upload

---
+++ portfolio_latest_js.js
@@ -0,0 +1,183 @@
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// protfolio filters
+$(window).on("load", function() {
+    var t = $(".portfolio-container");
+    t.isotope({
+        filter: ".new",
+        animationOptions: {
+            duration: 750,
+            easing: "linear",
+            queue: !1
+        }
+    }), $(".filters a").click(function() {
+        $(".filters .active").removeClass("active"), $(this).addClass("active");
+        var i = $(this).attr("data-filter");
+        return t.isotope({
+            filter: i,
+            animationOptions: {
+                duration: 750,
+                easing: "linear",
+                queue: !1
+            }
+        }), !1
+    });
+});
+
+// scroller 
+window.onscroll = function() { updateProgressBar(); };
+
+function updateProgressBar() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const dynamicTextElement = document.getElementById("dynamic-text");
+  const textArray = [" Web Designer", " Web Developer", " Front End Developer", " Apps Designer", " Apps Developer"]; // Array of texts
+  const typingDelay = 100; // Adjust the speed of typing (in milliseconds)
+  const erasingDelay = 50; // Adjust the speed of erasing (in milliseconds)
+  const newTextDelay = 2000; // Delay between typing each text (in milliseconds)
+  
+  let textArrayIndex = 0;
+  let charIndex = 0;
+
+  function typeText() {
+      if (charIndex < textArray[textArrayIndex].length) {
+          dynamicTextElement.textContent += textArray[textArrayIndex].charAt(charIndex);
+          charIndex++;
+          setTimeout(typeText, typingDelay);
+      } else {
+          setTimeout(eraseText, newTextDelay);
+      }
+  }
+
+  function eraseText() {
+      if (charIndex > 0) {
+          dynamicTextElement.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+          charIndex--;
+          setTimeout(eraseText, erasingDelay);
+      } else {
+          textArrayIndex++;
+          if (textArrayIndex >= textArray.length) {
+              textArrayIndex = 0;
+          }
+          setTimeout(typeText, typingDelay);
+      }
+  }
+
+  setTimeout(typeText, newTextDelay);
+});
+
+// google maps
+function initMap() {
+// Styles a map in night mode.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.674, lng: -73.945},
+        zoom: 12,
+        scrollwheel:  false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+      styles: [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#6b9a76'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+      ]
+    });
+}
+
+
